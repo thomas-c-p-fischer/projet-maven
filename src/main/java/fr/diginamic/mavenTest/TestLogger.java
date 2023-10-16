5ef60a309698041deb4f89d5e2b8c7bd8288e521
thomas.fischer <thomas.fischer@sully-group.com>
2023-10-16 16:38:03 +0200

    TP maven

---
+++ src/main/java/fr/diginamic/mavenTest/TestLogger.java
@@ -0,0 +1,15 @@
+package fr.diginamic.mavenTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogger {
+
+	private static final Logger LOG = LoggerFactory.getLogger(TestLogger.class);
+	
+	public static void main(String[] args) {
+		
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implémentation Logback");
+	}
+}
